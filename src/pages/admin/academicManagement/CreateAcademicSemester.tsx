afc357854b2509018e1e29e755b384c35e0fb21c
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-24 02:46:46 +0600

    up:CreateAcademicSemester

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,23 +1,21 @@
 import {  FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button, Col } from "antd";
+import { Button, Col, Flex } from "antd";
 
 export default function CreateAcademicSemester() {
   const onSubmit: SubmitHandler<FieldValues> = (data)=>{
     console.log(data)
   }
   return (
-
-    <Col col-6>
+    <Flex align="center" justify="center">
+    <Col span={6}>
     <PHForm onSubmit={onSubmit}>
       <PHInput type="text" name="name" label="Name"/>
       <PHInput type="text" name="year" label="Year"/>
-      <PHInput type="text" name="name" label="Name"/>
-      <PHInput type="text" name="name" label="Name"/>
-      <PHInput type="text" name="name" label="Name"/>
       <Button htmlType="submit">submit</Button>
     </PHForm>
     </Col>
+    </Flex>
   )
 }
